163d76001aed4bef6c650734148abbe7029d5023
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-12-15 12:02:10 +0200

    espresso error

--- main.py
+++ main.py
@@ -30,3 +30,41 @@ resources = {
     "coffee": 100,
 }
 
+
+def check_resources(user_input, resources_in_machine):
+    if MENU[user_input]["ingredients"]["water"] < resources_in_machine["water"]:
+        if MENU[user_input]["ingredients"]["coffee"] < resources_in_machine["coffee"]:
+            if MENU[user_input]["ingredients"]["milk"] < resources_in_machine["milk"]:
+                return "we can do this"
+            else:
+                return "no enough milk in machine"
+        else:
+            return  "no enough coffee in machine"
+    else:
+        return "no enough water in machine"
+
+def off_process():
+    return False
+
+process = True
+
+while process:
+    answer = input("What would you like?(espresso, latte, cappuccino): ")
+    money = 0
+
+    if answer == "off":
+       process = off_process()
+
+    if answer == "report":
+        print(f"Water {resources["water"]}ml")
+        print(f"milk {resources["milk"]}ml")
+        print(f"coffee {resources["coffee"]}g")
+        print(f"money ${money}")
+
+    if answer == "espresso":
+        check_resources(answer, resources)
+
+
+
+
+
